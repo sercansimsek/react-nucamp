ed856864fe7248b27e29e9ca4a4973041b6d3acd
Sercan Şimşek <sercansimsek44@gmail.com>
2026-06-01 10:52:02 +0200

    bootstrap added

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,8 +1,10 @@
 export const GameCard = (props) => {
   return (
     <>
-      <h4>{props.game.name}</h4>
-      <p>{props.game.category}</p>
+      <div className="bg-light border p-4 m-2">
+        <h4>{props.game.name}</h4>
+        <p>{props.game.category}</p>
+      </div>
     </>
   );
 };
