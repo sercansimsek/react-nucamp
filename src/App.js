eb86377406a707bc76a82e8fc2031cf56da24e5c
Sercan Şimşek <sercansimsek44@gmail.com>
2026-06-04 10:08:54 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -6,26 +6,39 @@ import "./App.css";
 import { HomePage } from "./components/HomePage";
 import { RandomPage } from "./components/RandomPage";
 import { GamePage } from "./components/GamePage";
+import { Navbar, Container, NavbarBrand, Nav } from "react-bootstrap";
 
 function App() {
   const [gamesList, setGamesList] = useState(TEST_GAMES);
 
   return (
     <div className="App">
-      <h1>My Board Games</h1>
-      <Link to="/" className="btn-link me-3">
-        Home
-      </Link>
-      <Link to="/random" className="btn-link">
-        Random
-      </Link>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <NavbarBrand as={Link} to="/">
+            My Board Games
+          </NavbarBrand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/random">
+              Random
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
       <Routes>
         <Route path="/" element={<HomePage gamesList={gamesList} />}></Route>
         <Route
           path="/random"
           element={<RandomPage gamesList={gamesList} />}
         ></Route>
-        <Route path="/games/:gameId" element={<GamePage gamesList={gamesList}/>}></Route>
+        <Route
+          path="/games/:gameId"
+          element={<GamePage gamesList={gamesList} />}
+        ></Route>
       </Routes>
     </div>
   );
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,9 +1,13 @@
-export const GameCard = (props) => {
+import { Link } from "react-router-dom";
+
+export const GameCard = ({ game }) => {
+  const { name, category } = game;
   return (
     <>
       <div className="bg-light border p-4 m-2">
-        <h4>{props.game.name}</h4>
-        <p>{props.game.category}</p>
+        <h4>{name}</h4>
+        <p>{category}</p>
+        <Link to={`/games/${game.id}`}>Details</Link>
       </div>
     </>
   );
