a969bfa7bbb21cffa94790e1f9b06f79fb9b4586
Sercan Şimşek <sercansimsek44@gmail.com>
2026-06-04 09:44:07 +0200

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import { TEST_GAMES } from "./TEST_GAMES";
 import "./App.css";
 
 import { HomePage } from "./components/HomePage";
+import { RandomPage } from "./components/RandomPage";
 
 function App() {
   const [gamesList, setGamesList] = useState(TEST_GAMES);
@@ -10,8 +12,19 @@ function App() {
   return (
     <div className="App">
       <h1>My Board Games</h1>
-
-      <HomePage gamesList={gamesList} />
+      <Link to="/" className="btn-link me-3">
+        Home
+      </Link>
+      <Link to="/random" className="btn-link">
+        Random
+      </Link>
+      <Routes>
+        <Route path="/" element={<HomePage gamesList={gamesList} />}></Route>
+        <Route
+          path="/random"
+          element={<RandomPage gamesList={gamesList} />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/RandomPage.js
+++ src/components/RandomPage.js
@@ -1,3 +1,9 @@
-export const RandomPage = () => {
-  return <div>RandomPage</div>;
+export const RandomPage = ({ gamesList }) => {
+  const randomGame = gamesList[Math.floor(Math.random() * gamesList.length)];
+  return (
+    <div>
+      <h5>You should play...</h5>
+      <h2 className="display-1">{randomGame.name}</h2>
+    </div>
+  );
 };
